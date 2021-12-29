67d9330c4d32115b28083cee18e77f13fe0c15eb
Sébastien Pencreach <pencreach.sebatien@gmail.com>
2021-12-29 08:32:50 +0100

    refactor: lints import

--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import { Provider } from 'react-redux';
+import { store } from './store';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { store } from './store';
 
 ReactDOM.render(
   <React.StrictMode>
